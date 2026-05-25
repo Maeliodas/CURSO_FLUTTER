b36c74297e2055840953f510f07ccce427b53de6
Victor Manuel <adrianagreste18@gmail.com>
2026-05-25 02:29:55 -0600

    Eliminada la clase WhetherApi y sus dependencias, se simplificó la estructura de la aplicación del clima y se mejoró la gestión de datos en MyHomePage. (Video constructor futuro)

--- wheather/lib/modules/whetherAPpi.dart
+++ wheather/lib/modules/whetherAPpi.dart
@@ -1,243 +1,122 @@
-class WhetherApi {
-  String? type;
-  Metadata? metadata;
-  List<Features>? features;
-  List<num>? bbox;
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
 
-  WhetherApi({required this.type, required this.metadata, required this.features, required this.bbox});
-
-  WhetherApi.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
-    metadata = json['metadata'] != null
-        ? new Metadata.fromJson(json['metadata'])
-        : null;
-    if (json['features'] != null) {
-      features = <Features>[];
-      json['features'].forEach((v) {
-        features?.add(new Features.fromJson(v));
-      });
-    }
-    bbox = json['bbox'].cast<num>();
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['type'] = this.type;
-    final metadata = this.metadata;
-    if (metadata != null) {
-      data['metadata'] = metadata.toJson();
-    }
-    final features = this.features;
-    if (features != null) {
-      data['features'] = features.map((v) => v.toJson()).toList();
-    }
-    data['bbox'] = this.bbox;
-    return data;
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplicacion del clima',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Clima'),
+    );
   }
 }
 
-class Metadata {
-  num? generated;
-  String? url;
-  String? title;
-  num? status;
-  String? api;
-  num? count;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  Metadata(
-      {required this.generated,
-      required this.url,
-      required this.title,
-      required this.status,
-      required this.api,
-      required this.count});
+  final String title;
 
-  Metadata.fromJson(Map<String, dynamic> json) {
-    generated = json['generated'];
-    url = json['url'];
-    title = json['title'];
-    status = json['status'];
-    api = json['api'];
-    count = json['count'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['generated'] = this.generated;
-    data['url'] = this.url;
-    data['title'] = this.title;
-    data['status'] = this.status;
-    data['api'] = this.api;
-    data['count'] = this.count;
-    return data;
-  }
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class Features {
-  String? type;
-  Properties? properties;
-  Geometry? geometry;
-  String? id;
+class _MyHomePageState extends State<MyHomePage> {
 
-  Features({this.type, this.properties, this.geometry, this.id});
+  fetchPosts() async{
 
-  Features.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
-    properties = json['properties'] != null
-        ? new Properties.fromJson(json['properties'])
-        : null;
-    geometry = json['geometry'] != null
-        ? new Geometry.fromJson(json['geometry'])
-        : null;
-    id = json['id'];
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['type'] = this.type;
-    if (this.properties != null) {
-      data['properties'] = this.properties!.toJson();
-    }
-    if (this.geometry != null) {
-      data['geometry'] = this.geometry!.toJson();
-    }
-    data['id'] = this.id;
-    return data;
-  }
-}
+  @override
+  Widget build(BuildContext context) {
 
-class Properties {
-  num? mag;
-  String? place;
-  num? time;
-  num? updated;
-  num? tz;
-  String? url;
-  String? detail;
-  num? felt;
-  num? cdi;
-  num? mmi;
-  String? alert;
-  String? status;
-  num? tsunami;
-  num? sig;
-  String? net;
-  String? code;
-  String? ids;
-  String? sources;
-  String? types;
-  num? nst;
-  num? dmin;
-  num? rms;
-  num? gap;
-  String? magType;
-  String? type;
-  String? title;
+    return Scaffold(
+      backgroundColor: Color(0xFFFAFAFA),
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text(widget.title),
+      ),
+      body:Container(
+        child: FutureBuilder(
+          future: fetchPosts(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if(snapshot.data == null){
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                )
+              );
+            }
+            else if(snapshot.data!=null){
+              if(snapshot.data=="Lamentamos los inconvenientes, servidor en mantenimiento"){
+                return Container(
+                  child: Center(
+                    child: Text(snapshot.data),
+                  )
+                );
+              }
+              else{
+                return ListView.builder(
+          
+            itemBuilder: (BuildContext context,int index){
+              
+          return Container(
+            decoration: BoxDecoration(
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Color(0xFFE0E0E0),
+                    offset: Offset(0.5, 0.5),
+                    blurRadius: 10.0,
+                  ),
+                ],
+                shape: BoxShape.rectangle,
+                color: Color(0xFFFAFAFA),
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            margin: EdgeInsets.all(8),
+            padding: EdgeInsets.all(8),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width/6,
+                  child:
+                Container(
+                  
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                        color:Colors.red,
 
-  Properties(
-      {this.mag,
-      this.place,
-      this.time,
-      this.updated,
-      this.tz,
-      this.url,
-      this.detail,
-      this.felt,
-      this.cdi,
-      this.mmi,
-      this.alert,
-      this.status,
-      this.tsunami,
-      this.sig,
-      this.net,
-      this.code,
-      this.ids,
-      this.sources,
-      this.types,
-      this.nst,
-      this.dmin,
-      this.rms,
-      this.gap,
-      this.magType,
-      this.type,
-      this.title});
+                  ),
+                  child:Container(
+                    margin: EdgeInsets.only(left:MediaQuery.of(context).size.width/19),
+                    child:Text("1",style: TextStyle(color:Colors.white,fontSize: 16,fontWeight: FontWeight.w700),))
+                )),
 
-  Properties.fromJson(Map<String, dynamic> json) {
-    mag = json['mag'];
-    place = json['place'];
-    time = json['time'];
-    updated = json['updated'];
-    tz = json['tz'];
-    url = json['url'];
-    detail = json['detail'];
-    felt = json['felt'];
-    cdi = json['cdi'];
-    mmi = json['mmi'];
-    alert = json['alert'];
-    status = json['status'];
-    tsunami = json['tsunami'];
-    sig = json['sig'];
-    net = json['net'];
-    code = json['code'];
-    ids = json['ids'];
-    sources = json['sources'];
-    types = json['types'];
-    nst = json['nst'];
-    dmin = json['dmin'];
-    rms = json['rms'];
-    gap = json['gap'];
-    magType = json['magType'];
-    type = json['type'];
-    title = json['title'];
-  }
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  children:<Widget>[
+                   Text("Tuxtepec",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w700)),
+                    Text("15 Km de Tuxtepec",style: TextStyle(fontSize: 12))
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['mag'] = this.mag;
-    data['place'] = this.place;
-    data['time'] = this.time;
-    data['updated'] = this.updated;
-    data['tz'] = this.tz;
-    data['url'] = this.url;
-    data['detail'] = this.detail;
-    data['felt'] = this.felt;
-    data['cdi'] = this.cdi;
-    data['mmi'] = this.mmi;
-    data['alert'] = this.alert;
-    data['status'] = this.status;
-    data['tsunami'] = this.tsunami;
-    data['sig'] = this.sig;
-    data['net'] = this.net;
-    data['code'] = this.code;
-    data['ids'] = this.ids;
-    data['sources'] = this.sources;
-    data['types'] = this.types;
-    data['nst'] = this.nst;
-    data['dmin'] = this.dmin;
-    data['rms'] = this.rms;
-    data['gap'] = this.gap;
-    data['magType'] = this.magType;
-    data['type'] = this.type;
-    data['title'] = this.title;
-    return data;
-  }
-}
+                  ]
+                  ),
+                )
+              ],
+            ),
+          );
+    });
+              }
+            }
+            throw UnimplementedError();
+          },
+          ) 
+    ));
 
-class Geometry {
-  String? type;
-  List<num>? coordinates;
-
-  Geometry({this.type, this.coordinates});
-
-  Geometry.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
-    coordinates = json['coordinates']?.cast<num>();
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['type'] = this.type;
-    data['coordinates'] = this.coordinates;
-    return data;
+    
   }
 }
