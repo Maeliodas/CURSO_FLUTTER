e11bfe42208306999a6a5d413be4575d019626df
Victor Manuel <adrianagreste18@gmail.com>
2026-05-24 23:27:30 -0600

    Añadido método para generar un número aleatorio y actualizado el texto en SecondClass para mostrar el número generado.

--- first_flutter_project/lib/second_file.dart
+++ first_flutter_project/lib/second_file.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SecondClass extends StatelessWidget {
@@ -7,9 +9,14 @@ class SecondClass extends StatelessWidget {
     return Material(
           color: Colors.yellow,
           child: Center(
-            child: Text("Esto es eso", textDirection: TextDirection.ltr,)
+            child: Text('Un numero random del 0 al 20 es ${generateNumbers()}', textDirection: TextDirection.ltr,)
           )
         );
   }
 
+  int generateNumbers(){
+    var r=Random();
+    int i=r.nextInt(20);
+    return i;
+  }
 }
