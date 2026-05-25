d30d22b10d8db59fce53f0de41afc7428d2ebf15
Victor Manuel <adrianagreste18@gmail.com>
2026-05-25 00:36:47 -0600

    Eliminada la alineación y decoración del contenedor en SecondClass para simplificar el código y mantener la consistencia visual.

--- first_flutter_project/lib/second_file.dart
+++ first_flutter_project/lib/second_file.dart
@@ -7,31 +7,45 @@ class SecondClass extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Material(
-          color: Colors.yellow,
-          child: Container(
+          //color: Colors.yellow,
+          child: SingleChildScrollView(
+             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+
+                Icon(Icons.ac_unit),
+          Container(
             height: 500.0,
             width: 300.0,
-            alignment: Alignment(0,0),
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink],
-                stops: [0.2, 0.5, 0.7, 1],
-                center: Alignment(0.1, 0.2),
-                focal: Alignment(-0.1, 0.3)
-              )
-            ),
-            //color: lightPurpleColor,
-              child: GestureDetector(
-                onTap: (){
-                  ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
-                    content: new Text("Presionado"),
-                    duration: Duration(seconds: 2),
-                  )
-                  );            
-                },
-                 child: Text(generateNumbers(), textDirection: TextDirection.ltr,
-                 )
-              )
+            // alignment: Alignment(0,0),
+            // decoration: BoxDecoration(
+            //   gradient: RadialGradient(
+            //     colors: [Colors.green, Colors.blue, Colors.orange, Colors.pink],
+            //     stops: [0.2, 0.5, 0.7, 1],
+            //     center: Alignment(0.1, 0.2),
+            //     focal: Alignment(-0.1, 0.3)
+            //   )
+            // ),
+             color: lightPurpleColor,
+            //   child: GestureDetector(
+            //     onTap: (){
+            //       ScaffoldMessenger.of(context).showSnackBar(new SnackBar(
+            //         content: new Text("Presionado"),
+            //         duration: Duration(seconds: 2),
+            //       )
+            //       );            
+            //     },
+            //      child: Text(generateNumbers(), textDirection: TextDirection.ltr, style: TextStyle(color: Colors.white,fontSize: 15.0, fontWeight: FontWeight.bold)
+            //      )
+              //)
+          ),
+          Container(
+            height: 500.0,
+            width: 100.0,
+            color: Colors.green,
+          )
+          ], 
+          )
           )
         );
   }
